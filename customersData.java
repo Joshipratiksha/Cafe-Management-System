df6322ccb76f6cade0ab7dd8216087d97f891b87
Pratiksha Joshi <65124698+Joshipratiksha@users.noreply.github.com>
2024-11-11 00:02:38 +0530

    updated files

---
+++ customersData.java
@@ -0,0 +1,56 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cafeshopmanagementsystem;
+
+/**
+ *
+ * @author ADMIN
+ */
+
+
+import java.sql.Date;
+
+/**
+ *
+ * @author WINDOWS 10
+ */
+public class customersData {
+
+    private Integer id;
+    private Integer customerID;
+    private Double total;
+    private Date date;
+    private String emUsername;
+
+    public customersData(Integer id, Integer customerID, Double total,
+             Date date, String emUsername) {
+        this.id = id;
+        this.customerID = customerID;
+        this.total = total;
+        this.date = date;
+        this.emUsername = emUsername;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public Integer getCustomerID() {
+        return customerID;
+    }
+
+    public Double getTotal() {
+        return total;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public String getEmUsername() {
+        return emUsername;
+    }
+
+}
